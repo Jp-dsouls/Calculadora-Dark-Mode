a83ae7c11437590ebefdb3b95960a372f8b07446
Junior Pezantes <werras04@gmail.com>
2022-09-29 15:33:50 -0500

    darkMode

---
+++ main.js
@@ -0,0 +1,34 @@
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    }
+    else if (item.id == "backSpace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    }
+    else if (display.innerText !== "" && item.id == "equals") {
+      display.innerText = eval(display.innerText);
+    }
+    else if (display.innerText !== "" && item.id == "equals") {
+      display.innerText = "Null";
+      setTimeout(() => (display.innerText = ""), 2000);
+    }
+    else {
+      display.innerText += item.id;
+    }
+  };
+});
+
+
+const themeBtnToggle = document.querySelector('.theme-toggler');
+const calculator = document.querySelector('.calculator');
+let isDark = true;
+themeBtnToggle.onclick = () => {
+  calculator.classList.toggle('dark');
+  themeBtnToggle.classList.toggle('active');
+  isDark = !isDark;
+}
